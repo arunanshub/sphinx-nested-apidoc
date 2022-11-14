69f52dbafd6c56a8c9d45652737a1a9e48804010
Arunanshu Biswas <mydellpc07@gmail.com>
2022-11-14 19:24:13 +0530

    test(core): add test for `get_nested_dir_filename`

---
+++ tests/__init__.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import os
+
+from hypothesis import strategies as st
+from hypothesis_fspaths import fspaths
+
+
+@st.composite
+def dotted_filenames(draw: st.DrawFn, maxdepth: int = 15) -> str:
+    """Generates sphinx-apidoc like filenames."""
+    # folder that contains the sphinx-apidoc file
+    folder_name = os.fsdecode(draw(fspaths(False)))
+    # build a sphinx-apidoc equivalent filename
+    text_st = st.text(
+        st.characters(blacklist_characters=[".", "/", "\\"]),
+        min_size=1,
+    )
+    filename = draw(text_st)
+    for _ in range(maxdepth):
+        filename += f".{draw(text_st)}"
+    full_path = os.path.join(folder_name, filename)
+    # add extension to file
+    extension = draw(text_st | st.none())
+    full_path = (
+        f"{full_path}{os.path.extsep}{extension}"
+        if extension is not None
+        else full_path
+    )
+    return full_path
--- tests/test_core.py
+++ tests/test_core.py
@@ -7,8 +7,20 @@ from hypothesis_fspaths import fspaths
 
 from sphinx_nested_apidoc import core
 
+from . import dotted_filenames
+
 
 @given(path=fspaths())
 def test_file_path(path: str):
     sanitized_path = core.sanitize_path(os.fsdecode(path))
     assert not os.path.isabs(sanitized_path)
+
+
+@given(dotted_filenames())
+def test_get_nested_dir_filename(filename: str):
+    nested = core.get_nested_dir_filename(filename)
+    path, ext = os.path.splitext(nested)
+    # the only `.` in nested path is the extension separator.
+    assert not path.count(".")
+    # the extensions of filename and nested path should match.
+    assert os.path.splitext(filename)[-1] == ext
