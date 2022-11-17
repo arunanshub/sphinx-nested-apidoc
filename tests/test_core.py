ea89584851f862af4b4e081d535e690288a59d2e
Arunanshu Biswas <mydellpc07@gmail.com>
2022-11-17 19:58:34 +0530

    test(core): add test for `feed_sphinx_apidoc`

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import os
+import pathlib
 
 from hypothesis import given
 from hypothesis_fspaths import fspaths
+from pytest_mock import MockerFixture
 
 from sphinx_nested_apidoc import core
 
@@ -24,3 +26,108 @@ def test_get_nested_dir_filename(filename: str):
     assert not path.count(".")
     # the extensions of filename and nested path should match.
     assert os.path.splitext(filename)[-1] == ext
+
+
+class TestFeedSphinxApidocFunc:
+    def test_help_in_argument(
+        self,
+        mocker: MockerFixture,
+        tmp_path: pathlib.Path,
+    ):
+        apidoc_main = mocker.patch("sphinx_nested_apidoc.core.apidoc.main")
+
+        output_dir = os.fspath(tmp_path / "output")
+        module_path = os.fspath(tmp_path / "module")
+
+        for arg in "-h", "--help":
+            core.feed_sphinx_apidoc(output_dir, module_path, arg)
+            apidoc_main.assert_called_with(
+                [
+                    "--output-dir",
+                    output_dir,
+                    module_path,
+                    "--separate",
+                    "--suffix",
+                    "rst",
+                    arg,
+                ]
+            )
+
+    def test_implicit_namespaces_argument(
+        self,
+        mocker: MockerFixture,
+        tmp_path: pathlib.Path,
+    ):
+        apidoc_main = mocker.patch("sphinx_nested_apidoc.core.apidoc.main")
+
+        output_dir = os.fspath(tmp_path / "output")
+        module_path = os.fspath(tmp_path / "module")
+
+        core.feed_sphinx_apidoc(
+            output_dir,
+            module_path,
+            implicit_namespaces=True,
+        )
+        apidoc_main.assert_called_once_with(
+            [
+                "--output-dir",
+                output_dir,
+                module_path,
+                "--separate",
+                "--suffix",
+                "rst",
+                "--implicit-namespaces",
+            ]
+        )
+
+    def test_force_argument(
+        self,
+        mocker: MockerFixture,
+        tmp_path: pathlib.Path,
+    ):
+        apidoc_main = mocker.patch("sphinx_nested_apidoc.core.apidoc.main")
+
+        output_dir = os.fspath(tmp_path / "output")
+        module_path = os.fspath(tmp_path / "module")
+
+        core.feed_sphinx_apidoc(
+            output_dir,
+            module_path,
+            force=True,
+        )
+        apidoc_main.assert_called_with(
+            [
+                "--output-dir",
+                output_dir,
+                module_path,
+                "--separate",
+                "--suffix",
+                "rst",
+                "--force",
+            ]
+        )
+
+    def test_dry_run_flag_removed(
+        self,
+        mocker: MockerFixture,
+        tmp_path: pathlib.Path,
+    ):
+        apidoc_main = mocker.patch("sphinx_nested_apidoc.core.apidoc.main")
+
+        output_dir = os.fspath(tmp_path / "output")
+        module_path = os.fspath(tmp_path / "module")
+
+        for arg in "-n", "--dry-run":
+            core.feed_sphinx_apidoc(output_dir, module_path, arg)
+            apidoc_main.assert_called_with(
+                [
+                    "--output-dir",
+                    output_dir,
+                    module_path,
+                    "--separate",
+                    "--suffix",
+                    "rst",
+                ]
+            )
+
+    # TODO: check working of _add_flag_if_not_present
