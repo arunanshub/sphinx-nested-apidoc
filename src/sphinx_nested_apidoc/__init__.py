5399a9fbba1068a48c60b73daf3a8e374d5fdfe1
Arunanshu Biswas <48434243+arunanshub@users.noreply.github.com>
2023-10-16 13:39:37 +0530

    bump: version 1.2.4 → 1.2.5 (#217)

--- src/sphinx_nested_apidoc/__init__.py
+++ src/sphinx_nested_apidoc/__init__.py
@@ -6,7 +6,7 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 LOGGER_FORMAT = "[{levelname}: {filename}:{lineno}]: {message}"
 
-__version__ = "1.2.4"
+__version__ = "1.2.5"
 
 
 def start_logging(
